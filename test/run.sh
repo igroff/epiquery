824a4d7e520acbd6ed8aeca561fcbae3db5a2e16
Ian Groff <ian.groff@you_should_know.com>
2013-04-18 16:20:33 -0500

    deep version of 'escape for sql', you may also call it 'correct-er'

--- test/run.sh
+++ test/run.sh
@@ -3,7 +3,7 @@ TEST_URL=http://localhost:${EPIQUERY_HTTP_PORT-8080}
 echo Using ${TEST_URL} for testing
 function run_test() {
   PATH_NAME=`echo $1 | sed -e s[?.*[[g`
-  curl -s "${TEST_URL}/test/$1" > tmp/$PATH_NAME.result 
+  curl -s "${TEST_URL}/test/$1" "${@:1}" > tmp/$PATH_NAME.result 
   diff data/$PATH_NAME.expected tmp/$PATH_NAME.result
   DIFF_RESULT=$?
   printf "Test %s " ${PATH_NAME}
@@ -44,6 +44,7 @@ run_test 'echo.dot?message=pants&dog=blue'
 run_test prepare_a_statement
 run_test 'prepare_a_statement_with_param.mustache?id=1'
 run_test do_stuff_but_return_no_results
+run_test array_in_context.mustache --data-binary '{"items":[{"name": "one"}, {"name": "two"}]}' -H 'Content-Type: application/json'
 
 run_dynamic_test dynamic1 'select 1 [column]'
 run_dynamic_test dynamic2 "select 'Hello, '+'{{name}}' [message]" --data-urlencode 'name=ian'
