c6fcdcf4defd3f5f7848d1025ff293122ae8a0c3
Ian Groff <ian.groff@you_should_know.com>
2013-05-15 13:44:55 -0400

    adding tests

--- test/run.sh
+++ test/run.sh
@@ -3,7 +3,7 @@ TEST_URL=http://localhost:${EPIQUERY_HTTP_PORT-6060}
 echo Using ${TEST_URL} for testing
 function run_test() {
   PATH_NAME=`echo $1 | sed -e s[?.*[[g`
-  curl -X ${METHOD-GET} -s "${TEST_URL}/test/$1" "${@:1}" > tmp/$PATH_NAME.result 
+  curl -X ${METHOD-GET} -s "${TEST_URL}/test/$1" ${@:2} > tmp/$PATH_NAME.result
   diff data/$PATH_NAME.expected tmp/$PATH_NAME.result
   DIFF_RESULT=$?
   printf "Test %s " ${PATH_NAME}
@@ -14,9 +14,16 @@ function run_test() {
   fi
 }
 function do_post(){
-	export METHOD=POST
-	run_test $@
-	export METHOD=GET
+  PATH_NAME=`echo $1 | sed -e s[?.*[[g`
+  curl -X POST -s "${TEST_URL}/test/$1" ${@:2} > tmp/$PATH_NAME.result
+  diff data/$PATH_NAME.expected tmp/$PATH_NAME.result
+  DIFF_RESULT=$?
+  printf "Test %s " ${PATH_NAME}
+  if [ $DIFF_RESULT -eq 0 ]; then
+    echo "success"
+  else
+    echo "failed"
+  fi
 }
 function run_dynamic_test(){
   # development query tests
@@ -49,10 +56,11 @@ do_post 'echo.dot?message=pants&dog=blue'
 do_post prepare_a_statement
 do_post 'prepare_a_statement_with_param.mustache?id=1'
 run_test do_stuff_but_return_no_results
-run_test array_in_context.mustache --data-binary '{"items":[{"name": "one"}, {"name": "two"}]}' -H 'Content-Type: application/json'
 run_test mysql_simple_select
 # GETs are going to be read uncommitted
 run_test mysql_txi_level 
+do_post app_json_mysql_hello.mustache --data-binary '{"name":"ian"}' -H 'Content-Type:application/json'
+run_test array_in_context.mustache --data-binary '{"items":[{"name":"one"},{"name":"two"}]}' -H 'Content-Type:application/json'
 
 # want to make sure we get something simliar with and without a specific connection
 do_post mysql_multiple_row_select
