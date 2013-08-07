a435557e9b32d6404f5c5f636ec43b909ff4d8c6
Ian Groff <ian.groff@you_should_know.com>
2013-08-07 16:33:28 -0400

    more WIP

---
+++ samples/results1.js
@@ -0,0 +1,27 @@
+angular.callbacks._0({
+  "templateName":"",
+  "__template":"",
+  "input": {},
+  "resultSets": [
+      {
+          "metadata": {"columnNames":["id","name"], "rowCount":3},
+          "rows": [
+            {"id":1, "name": "Ian"},
+            {"id":2, "name": "David"},
+            {"id":3, "name": "George"} 
+          ]
+      },
+      {
+          "metadata": {"columnNames":["id","name"], "rowCount":6},
+          "rows": [
+            {"id":1, "name": "Ian"},
+            {"id":2, "name": "David"},
+            {"id":3, "name": "George"},
+            {"id":4, "name": "Carolyn"},
+            {"id":5, "name": "Sayeef"},
+            {"id":6, "name": "Priya"}
+          ]
+      }
+  ],
+  "metadata": {"resultSetCount":2}
+});
